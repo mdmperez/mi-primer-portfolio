7c2e0a890adc9bae0501b3941519f861d616e0b1
María del Mar <mdmperez91@gmail.com>
2020-03-05 22:06:15 +0100

    add

---
+++ js.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("header__nav");
+var sticky = header__nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("fixed-nav")
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+}
