37f2db9c80a14b019e91805503a01803c15c3cde
María del Mar <mdmperez91@gmail.com>
2020-03-17 12:35:50 +0100

    Añadiendo javascript a los tooltips de experiencia

--- js.js
+++ js.js
@@ -1,14 +1,33 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollActivate()};
 
 var navbar = document.getElementById("header__nav");
 var sticky = header__nav.offsetTop;
 
-function myFunction() {
+function scrollActivate() {
   if (window.pageYOffset > sticky) {
     navbar.classList.add("fixed-nav")
   } else {
     navbar.classList.remove("fixed-nav");
   }
 }
+
+//Botones tooltips
+
+const btn1 = document.querySelector('.btn-tooltip1');
+const tooltip1 = document.querySelector('.tooltip1');
+const closeElement = document.querySelector('.experience_list__tooltip--close');
+
+function openTooltip(event) {
+  let addClass = event.currentTarget;
+  tooltip1.classList.toggle('open-tooltip');
+}
+function closeTooltip(event) {
+  let addClass = event.currentTarget;
+  tooltip1.classList.toggle('open-tooltip');
+}
+btn1.addEventListener('click', openTooltip);
+closeElement.addEventListener('click', openTooltip);
+
+
 
 
